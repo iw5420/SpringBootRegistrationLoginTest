6b0d25907caafa707d52cf8213b11f1ecfda55b9
Ian <iw5420@gmail.com>
2021-05-29 16:18:12 +0800

    first commit

---
+++ src/test/java/com/springsecurity/SpringBootRegistrationLogin/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.SpringBootRegistrationLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
